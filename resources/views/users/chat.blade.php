fcd57d9fc5b8da0cdd3f494319aed06e339e1a6c
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-07-24 05:56:25 +0000

    almost

--- resources/views/users/chat.blade.php
+++ resources/views/users/chat.blade.php
@@ -11,11 +11,11 @@
            
                   {!! Form::open(['route' => 'postings.store']) !!}
                       <div class="form-group">
-                          {!! Form::label('chatmonotitle', '欲しいモノのお名前:') !!}
+                          {!! Form::label('chatmonotitle', '宛先（例：＞〇〇〇〇さん）:') !!}
                           {!! Form::textarea('chatmonotitle', old('chatmonotitle'), ['class' => 'form-control', 'rows' => '2']) !!}
                           
                           <input type="hidden" name="mono_id" value="{{$mono_id}}"/>
-                          {!! Form::label('title', 'お問い合わせ内容（返信の際は宛先を忘れずに！ 例：＞〇〇〇〇さん）:') !!}
+                          {!! Form::label('title', 'お問い合わせ内容:') !!}
                           {!! Form::textarea('saying', old('saying'), ['class' => 'form-control', 'rows' => '2']) !!}
                           {!! Form::submit('Post', ['class' => 'btn btn-success btn-block']) !!}
                       </div>
